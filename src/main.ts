500cdd1b1844625e72551f75c7eba913dcfa3fca
naeemudheen p <nayeemudheenaslam3@gmail.com>
2024-12-27 14:48:26 +0530

    Enabled cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: '*', // Allow all origins (not recommended for production)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true, // Allow cookies and credentials
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
